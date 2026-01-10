175d2b9a5d60433b4e7e919b0341f1ed47e5be20
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-10 08:26:48 -0300

    fix: ponto de referência principal

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,37 +1,39 @@
-import Faq from "../components/sections/Faq";
-import About from "../components/sections/About";
-import BackToTopButton from "../components/interactives/BackToTopButton";
-import FloatingWhatsappButton from "../components/interactives/FloatingWhatsappButton";
-import HeroBgImg from "../components/sections/HeroBgImg";
-import FooterSocial from "../components/sections/FooterSocial";
-import NavbarSocial from "../components/sections/NavbarSocial";
-import HowItWorks from "../components/sections/HowItWorks";
-import FeaturesWithIcons from "../components/sections/FeaturesWithIcons";
-import Numbers from "../components/sections/Numbers";
-import AboutInstagram from "../components/sections/AboutInstagram";
-import HowItWorksImage from "../components/sections/HowItWorksImage";
-import CtaMinimal from "../components/sections/CtaMinimal";
-import Depositions from "../components/sections/Depositions";
-import FeaturesButton from "../components/sections/FeaturesButton";
+import Faq from '../components/sections/Faq'
+import About from '../components/sections/About'
+import BackToTopButton from '../components/interactives/BackToTopButton'
+import FloatingWhatsappButton from '../components/interactives/FloatingWhatsappButton'
+import HeroBgImg from '../components/sections/HeroBgImg'
+import FooterSocial from '../components/sections/FooterSocial'
+import NavbarSocial from '../components/sections/NavbarSocial'
+import HowItWorks from '../components/sections/HowItWorks'
+import FeaturesWithIcons from '../components/sections/FeaturesWithIcons'
+import Numbers from '../components/sections/Numbers'
+import AboutInstagram from '../components/sections/AboutInstagram'
+import HowItWorksImage from '../components/sections/HowItWorksImage'
+import CtaMinimal from '../components/sections/CtaMinimal'
+import Depositions from '../components/sections/Depositions'
+import FeaturesButton from '../components/sections/FeaturesButton'
 
 export default function Index() {
   return (
     <div>
       <NavbarSocial />
-      <HeroBgImg />
-      <FeaturesButton />
-      {/* <FeaturesWithIcons /> */}
-    
-      <HowItWorksImage />
-      {/* <Numbers /> */}
-      <About modal={true} />
-      <AboutInstagram />
-      <CtaMinimal />
-      {/* <Depositions /> */}
-      <Faq />
-      <FooterSocial />
-      <FloatingWhatsappButton />
-      <BackToTopButton />
+      <main>
+        {' '}
+        <HeroBgImg />
+        <FeaturesButton />
+        {/* <FeaturesWithIcons /> */}
+        <HowItWorksImage />
+        {/* <Numbers /> */}
+        <About modal={true} />
+        <AboutInstagram />
+        <CtaMinimal />
+        {/* <Depositions /> */}
+        <Faq />
+        <FooterSocial />
+        <FloatingWhatsappButton />
+        <BackToTopButton />
+      </main>
     </div>
-  );
+  )
 }
